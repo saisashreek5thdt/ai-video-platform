6a0778c3ed13ccc9a9b0b262b1e7bbe40b4e46cd
Sai Sashreek D <sai@5thdt.com>
2024-11-07 17:35:29 +0530

    New Routes Added

---
+++ app/api/image-upload/route.ts
@@ -0,0 +1,66 @@
+import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+import { auth } from "@clerk/nextjs/server";
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+interface CloudinaryUploadResult {
+    public_id: string;
+    [key: string]: any;
+}
+
+export async function POST(request: NextRequest) {
+    const { userId } = auth();
+
+    if (!userId) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    if (!process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || process.env.CLOUDINARY_API_SECRET) {
+        return NextResponse.json({error: "Cloudinary Credentials Not Found"}, {status: 500})
+    }
+
+    try {
+        const formData = await request.formData();
+        const file = formData.get("file") as File | null;
+
+        if (!file) {
+            return NextResponse.json(
+                { error: "File Not Found" },
+                { status: 400 }
+            );
+        }
+
+        const bytes = await file.arrayBuffer();
+        const buffer = Buffer.from(bytes);
+
+        const result = await new Promise<CloudinaryUploadResult>(
+            (resolve, reject) => {
+                const uploadStream = cloudinary.uploader.upload_stream(
+                    { folder: "sass-file-uploads" },
+                    (error, result) => {
+                        if (error) reject(error);
+                        else resolve(result as CloudinaryUploadResult);
+                    }
+                );
+                uploadStream.end(buffer);
+            }
+        );
+
+        return NextResponse.json(
+            { publicId: result.public_id },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.log("Upload Image Failed", error);
+        return NextResponse.json(
+            { error: "Upload Image Failed" },
+            { status: 500 }
+        );
+    }
+}
