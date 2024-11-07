af6eabe7c933d45dde11df0ec006b2c60bc9f49c
Sai Sashreek D <sai@5thdt.com>
2024-11-07 13:56:30 +0530

    Layout Page updated with ClerkProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
-  title: "AI Video Educational Platform",
-  description: "AI Supported Video Platform for every user, who wants to learn from platform.",
+    title: "AI Video Educational Platform",
+    description:
+        "AI Supported Video Platform for every user, who wants to learn from platform.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body>{children}</body>
+            </html>
+        </ClerkProvider>
+    );
 }
